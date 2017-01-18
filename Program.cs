24be30bba7f4d3da5fb8d203378b41c96c668860
Michael Kriese <michael.kriese@visualon.de>
2017-01-18 11:19:20 +0100

    Added obfuscation test.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+
+namespace Crypto
+{
+    [Obfuscation]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine($"Success: {Test()}");
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"Unexpected Error: {ex}");
+                Environment.Exit(-1);
+            }
+
+        }
+
+        [Obfuscation]
+        static int Test()
+        {
+            var states = new [] { TestState.NothingToDo, TestState.Success, TestState.Dummy };
+            return states.Length;
+        }
+    }
+}
---
+++ TestState.cs
@@ -0,0 +1,11 @@
+﻿namespace Crypto
+{
+    public enum TestState
+    {
+        Error = -1,
+        Unknown = 0,
+        NothingToDo = 1,
+        Success = 2,
+        Dummy = 10
+    }
+}
